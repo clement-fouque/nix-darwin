7fdaedc306e843f245a7866de412868ddf9387bf
Clément Fouque <67908012+clement-fouque@users.noreply.github.com>
2026-03-10 18:08:28 +0100

    Add elastic-package go installation

--- hosts/professional/home-manager.nix
+++ hosts/professional/home-manager.nix
@@ -4,4 +4,17 @@
   home.stateVersion = "24.05";
   home.username = username;
   home.homeDirectory = lib.mkForce "/Users/${username}";
+
+  home.sessionPath = [
+    "$HOME/go/bin"
+  ];
+
+  home.activation.installGoPackages = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    export PATH="/opt/homebrew/bin:$PATH"
+    ${builtins.concatStringsSep "\n" (map (pkg: ''
+      run go install ${pkg}
+    '') [
+      "github.com/elastic/elastic-package@latest"
+    ])}
+  '';
 }
